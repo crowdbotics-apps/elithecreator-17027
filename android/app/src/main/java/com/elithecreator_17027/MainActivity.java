8f7b86241df67f66d5107249594a5d9e6eb8d946
Crowdbotics <team@crowdbotics.com>
2020-05-14 23:03:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/elithecreator_17027/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elithecreator_17027;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "elithecreator_17027";
+    }
+}
